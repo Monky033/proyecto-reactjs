10d39e0ab91ccd809bb1ef1b65edcef8b53fdc72
juan ignacio moreno <juanimignacio@gmail.com>
2023-06-10 11:10:47 -0300

    subiendo los primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Juani Manda</h1>;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h1>lo hice?</h1>
+      <h5>-Si, lo hiciste.</h5>
+    </div>
+  );
+};
+
+export default Navbar;
