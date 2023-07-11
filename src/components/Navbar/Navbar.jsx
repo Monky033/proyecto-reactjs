08a6e5b36507295757f5e3e9f2b019da1e2cf2b9
juan ignacio moreno <juanimignacio@gmail.com>
2023-07-10 22:47:45 -0300

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const Stilo = styled.nav``;
+
+export default function Navbar() {
+  return (
+    <Stilo>
+      <h1>se pudo!!!!!</h1>
+    </Stilo>
+  );
+}
